94f628bbcc796f8154c9b2f6987142aa427c0411
kawakami-o3 <mikamihayato383@gmail.com>
2012-09-15 07:55:01 +0900

    合計値のみ保持するPath版

--- chapter10/heathrowToLondon3.hs
+++ chapter10/heathrowToLondon3.hs
@@ -11,32 +11,35 @@ type RoadSystem = [Section]
 --                   ]
 
 data Label = A | B | C deriving (Show)
-type Path = [(Label, Int)]
+--type Path = [(Label, Int)]
+type Path = ([Label],Int)
 
 roadStep :: (Path, Path) -> Section -> (Path, Path)
 roadStep (pathA, pathB) (Section a b c) =
-  let timeA = sum (map snd pathA)
-      timeB = sum (map snd pathB)
+  let timeA = snd pathA
+      timeB = snd pathB
       forwardTimeToA = timeA + a
       crossTimeToA = timeB + b + c
       forwardTimeToB = timeB + b
       crossTimeToB = timeA + a + c
       newPathToA = if forwardTimeToA <= crossTimeToA
-                      then (A,a):pathA
-                      else (C,c):(B,b):pathB
+                      then (A:(fst pathA), timeA+a)
+                      else (C:B:(fst pathB), timeB+b+c)
       newPathToB = if forwardTimeToB <= crossTimeToB
-                      then (B,b):pathB
-                      else (C,c):(A,a):pathA
+                      then (B:(fst pathB), timeB+b)
+                      else (C:A:(fst pathA), timeA+a+c)
+                      --then (B,b):pathB
+                      --else (C,c):(A,a):pathA
   in (newPathToA, newPathToB)
 
 
 
 optimalPath :: RoadSystem -> Path
 optimalPath roadSystem =
-  let (bestAPath, bestBPath) = foldl roadStep ([],[]) roadSystem
-  in if sum (map snd bestAPath) <= sum (map snd bestBPath)
-          then reverse bestAPath
-          else reverse bestBPath
+  let (bestAPath, bestBPath) = foldl roadStep (([],0),([],0)) roadSystem
+  in if (snd bestAPath) <= (snd bestBPath)
+          then (reverse (fst bestAPath), snd bestAPath)
+          else (reverse (fst bestBPath), snd bestBPath)
 
 groupsOf :: Int -> [a] -> [[a]]
 groupsOf 0 _ = undefined
@@ -48,8 +51,8 @@ main = do
   let threes = groupsOf 3 (map read $ lines contents)
       roadSystem = map (\[a,b,c] -> Section a b c) threes
       path = optimalPath roadSystem
-      pathString = concat $ map (show . fst) path
-      pathTime = sum $ map snd path
+      pathString = concat $ map (show) $ fst path
+      pathTime = snd path
   putStrLn $ "The best path to take is: " ++ pathString
   putStrLn $ "Time taken: " ++ show pathTime
 
